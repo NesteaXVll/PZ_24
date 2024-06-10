559c094a75989e6c572688abf6acbbb3681af217
Anastasia <thedevil_2@mail.ru>
2024-06-10 23:14:51 +0700

    Добавьте файлы проекта.

---
+++ PZ_24/Form2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PZ_24
+{
+    public partial class Colors : Form
+    {
+        
+        public Colors()
+        {
+            InitializeComponent();
+            Scroll_Red.Tag = numeric_Red;
+            Scroll_Green.Tag = numeric_Green;
+            Scroll_Blue.Tag = numeric_Blue;
+            numeric_Red.Tag = Scroll_Red;
+            numeric_Green.Tag = Scroll_Green;
+            numeric_Blue.Tag = Scroll_Blue;
+            numeric_Red.Value = color.R;
+            numeric_Green.Value = color.G;
+            numeric_Blue.Value = color.B;
+        }
+
+        private void Colors_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
